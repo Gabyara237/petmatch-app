8f279b19ab2d4199168afcdcd74742325a173623
Gabriela Araujo <gabyara237@gmail.com>
2025-06-06 16:46:26 -0700

    feat: initialize Spring Boot backend with PostgreSQL and .env support

---
+++ petmatch-backend/src/test/java/com/petmatch/petmatch_backend/PetmatchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petmatch.petmatch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmatchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
