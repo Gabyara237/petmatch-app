2fcaf1b6cf907f0dc32fb45892326d6dc39e17bb
Gabriela Araujo <gabyara237@gmail.com>
2025-06-11 12:07:38 -0700

    feat: initial structure with Spring Boot backend

---
+++ petmatch-backend/src/main/java/com/petmatch/petmatch_backend/PetmatchApp.java
@@ -0,0 +1,13 @@
+package com.petmatch.petmatch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmatchApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetmatchApp.class, args);
+	}
+
+}
---
+++ petmatch-backend/src/test/java/com/petmatch/petmatch_backend/PetmatchAppTests.java
@@ -0,0 +1,13 @@
+package com.petmatch.petmatch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmatchAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
